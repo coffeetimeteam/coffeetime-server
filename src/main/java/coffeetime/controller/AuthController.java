9aec7e8075f7e02acbe6866449b57095a4f3c881
changedan <hello@parkgadan.com>
2025-01-11 17:46:22 +0900

    refac: ♻️ update login logic[#5]

--- src/main/java/coffeetime/controller/AuthController.java
+++ src/main/java/coffeetime/controller/AuthController.java
@@ -6,18 +6,13 @@ import coffeetime.dto.AuthResponse;
 import coffeetime.dto.RefreshTokenRequest;
 import coffeetime.exception.CoffeeTimeException;
 import coffeetime.exception.EntryPayloadCode;
-import coffeetime.infrastructure.CustomUserDetails;
+import coffeetime.service.AuthService;
 import coffeetime.service.TokenService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
-import org.springframework.security.authentication.BadCredentialsException;
-import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -30,25 +25,17 @@ public class AuthController {
 
 	private final AuthenticationManager authenticationManager;
 	private final TokenService tokenService;
+	private final AuthService authService;
 
 	@PostMapping("/login")
-	public ResponseEntity<APIResponse> getAccessToken(
+	public ResponseEntity<APIResponse> getTokens(
 		@RequestBody @Valid AuthRequest request) {
-		try {
-			Authentication authentication = authenticationManager.authenticate(
-				new UsernamePasswordAuthenticationToken(request.username(),
-					request.password()));
-			CustomUserDetails userDetails = (CustomUserDetails) authentication.getPrincipal();
-			AuthResponse response = tokenService.generateTokens(
-				userDetails.user());
-			HttpHeaders tokenHeaders = generateToken(response.getAccessToken(),
-				response.getRefreshToken());
-			return ResponseEntity.ok().headers(tokenHeaders).body(new APIResponse(EntryPayloadCode.SUCCESS_REQUEST));
-		} catch (BadCredentialsException e) {
-			return ResponseEntity.status(HttpStatus.UNAUTHORIZED).body(new APIResponse(EntryPayloadCode.BAD_CREDENTIAL));
-		} catch (UsernameNotFoundException e) {
-			return ResponseEntity.status(HttpStatus.NOT_FOUND).body(new APIResponse(EntryPayloadCode.NOT_FOUND_USER));
-		}
+		AuthResponse response = authService.getTokens(request);
+		HttpHeaders headers = generateToken(response.getAccessToken(),
+			response.getRefreshToken());
+		return ResponseEntity.ok()
+			.headers(headers)
+			.body(new APIResponse(EntryPayloadCode.SUCCESS_REQUEST));
 	}
 
 	@PostMapping("/token")
