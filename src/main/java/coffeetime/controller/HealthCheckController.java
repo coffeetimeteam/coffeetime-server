9207bcf8b90fc39c907300eb83ea3c7f43300244
changedan <hello@parkgadan.com>
2025-01-15 18:26:28 +0900

    feat: ✨ health check controller[#23]

--- src/main/java/coffeetime/controller/HealthCheckController.java
+++ src/main/java/coffeetime/controller/HealthCheckController.java
@@ -1,13 +1,19 @@
 package coffeetime.controller;
 
+import coffeetime.dto.GlobalResponse;
+import coffeetime.exception.EntryPayloadCode;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@RequestMapping("/api")
 public class HealthCheckController {
 
 	@GetMapping("/health-check")
-	public String getHealthCheck() {
-		return "up";
+	public ResponseEntity<GlobalResponse> getHealthCheck() {
+		GlobalResponse response = new GlobalResponse(EntryPayloadCode.SUCCESS_REQUEST);
+		return ResponseEntity.ok(response);
 	}
 }
