b19b8f1b3e0960f30eb0923746ffb648309668f8
changedan <hello@parkgadan.com>
2025-01-25 17:19:22 +0900

    chore: 🔧 update a database setting[#31]

--- src/main/java/coffeetime/CoffeetimeServerApplication.java
+++ src/main/java/coffeetime/CoffeetimeServerApplication.java
@@ -1,5 +1,6 @@
 package coffeetime;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
@@ -10,6 +11,13 @@ public class CoffeetimeServerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CoffeetimeServerApplication.class, args);
-	}
 
+		HikariDataSource dataSource = new HikariDataSource();
+		Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+			if (!dataSource.isClosed()) {
+				dataSource.close();
+				System.out.println("HikariDataSource closed on JVM shutdown.");
+			}
+		}));
+	}
 }
